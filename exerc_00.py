b4fcefe2a6e1dd46936e4482d29ad0a70380705e
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-16 20:33:18 -0300

    f

---
+++ exerc_00.py
@@ -0,0 +1,29 @@
+frase = [curso em video Python]
+frase[9:21]
+len(frase)
+frase.count('o')
+frase.find('deo')
+frase.find('android')
+#Se não existe a srting ele retorna -1
+
+'curso' in frase
+#verifica se tem a palavra é um operador
+
+
+#transformação
+#metodos
+
+"""frase.replace('Pyhton','android')
+frase.uper()
+frase.lower()
+frase.capitalize()
+frase.title()"""
+
+frase = [Aprenda Python]
+
+frase.lstrip()
+#divisão
+frase.split()
+
+#junção
+'-'.join(frase)
---
+++ exerc_01.py
@@ -0,0 +1,5 @@
+frase ='Curso em video Python'
+print(frase[::2])
+print(frase.count('o'))
+print(frase.upper().count)
+print(len(frase.strip))
